ef05d078bbc604a11a32eae03c950240790ba6f9
halit yakici <halit.yakici.hy@gmail.com>
2025-09-17 23:44:37 +0300

    post ve pre increment eksik

---
+++ cpp02/ex02/main.cpp
@@ -0,0 +1,20 @@
+
+
+
+
+
+#include "Fixed.hpp"
+
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int main() {
+    
+    Fixed a( 2 );
+    Fixed b( 3.5f );
+
+    std::cout << (a++) << std::endl;
+
+    return 0;
+}
