534af301d43f80877b2208df32650d9701b2a486
halitykc <87850150+halitykc@users.noreply.github.com>
2026-08-12 18:27:13 +0000

    ex01

--- cpp05/ex01/Form.cpp
+++ cpp05/ex01/Form.cpp
@@ -12,17 +12,17 @@ const char* Form::GradeTooLowExeption::what() const throw() {
     return "Grade too Low";
 }
 
-Form::Form(void): _formName(""), _minGradeToSign(5), _minGradeToExec(10) {
+Form::Form(void): _formName(""), _minGradeToSign(5), _minGradeToExec(10), _signed(false) {
 
 }
 
-Form::Form(const std::string& name, const int minGradeS, const int minGradeToE) : _formName(name), _minGradeToSign(minGradeS), _minGradeToExec(_minGradeToExec) {
+Form::Form(const std::string& name, const int minGradeS, const int minGradeToE) : _formName(name), _minGradeToSign(minGradeS), _minGradeToExec(minGradeToExec), _signed(false) {
 
-    if
+
 
 }
 
-Form::Form(const Form& copy): _minGradeToExec(copy._minGradeToExec), _minGradeToSign(copy._minGradeToSign) {
+Form::Form(const Form& copy): _minGradeToExec(copy._minGradeToExec), _minGradeToSign(copy._minGradeToSign), _signed(false) {
 
 
 }
