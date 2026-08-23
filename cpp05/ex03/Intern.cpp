ff042c8bf2c67dd4b76373fc06c6e6f537354f9e
halitykc <87850150+halitykc@users.noreply.github.com>
2026-08-23 18:53:21 +0000

    klajsla

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -1,6 +1,8 @@
 
 #include "Intern.hpp"
-
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 
 Intern::Intern() {
@@ -24,4 +26,38 @@ Intern& Intern::operator=(const Intern &other) {
 Intern::~Intern() {
 
     std::cout << "Intern destructor called" << std::endl;
+}
+
+AForm* Intern::makeShrubberyCreationForm(const std::string &target) {
+
+    return new ShrubberyCreationForm(target);
+}
+
+AForm* Intern::makeRobotomyRequestForm(const std::string &target) {
+
+    return new RobotomyRequestForm(target);
+}
+
+AForm* Intern::makePresidentialPardonForm(const std::string &target) {
+
+    return new PresidentialPardonForm(target);
+}
+
+AForm* Intern::makeForm(const std::string &formName, const std::string &target) const {
+
+    std::string func_list[3] = {"shrubbery creation", "robotomy request", "presidential pardon"};
+    AForm* (*func_form[3])(const std::string &) = {
+        &Intern::makeShrubberyCreationForm,
+        &Intern::makeRobotomyRequestForm,
+        &Intern::makePresidentialPardonForm
+    };
+
+    for (int i = 0; i < 3; i++) {
+        if (formName == func_list[i]) {
+            std::cout << "Intern creates " << formName << std::endl;
+            return func_form[i](target);
+        }
+    }
+    std::cout << "Intern cannot create " << formName << " cus doesnt exist." << std::endl;
+    return nullptr;
 }
