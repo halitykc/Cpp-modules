e87cf4a49f551ee0ec2987829b7d5b7f0641f1de
halitykc <87850150+halitykc@users.noreply.github.com>
2026-09-03 12:27:38 +0000

    ksaldj

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char **argv) {
 
-    if (argc != 2)
+    if (argc != 2 || argv[1][0] == '\0')
     {
         std::cout << "Usage: ./convert <literal>" << std::endl;
         return 1;
