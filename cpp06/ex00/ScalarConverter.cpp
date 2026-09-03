03d516f41138e152ad1abbe99fbca87d2988d19e
halitykc <87850150+halitykc@users.noreply.github.com>
2026-09-03 12:14:39 +0000

    jkadsaj

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -3,7 +3,7 @@
 #include "ScalarConverter.hpp"
 
 
-bool isSpecialLiteral(const std::string &text) {
+static bool isSpecialLiteral(const std::string &text) {
     std::string ex_literals[3] = {"nan", "+inf", "-inf"};
     std::string ex_scien_literals[3] = {"nanf", "+inff", "-inff"};
    
@@ -22,15 +22,27 @@ bool isSpecialLiteral(const std::string &text) {
     return false;
 }
 
+static bool isChar(const std::string &text) {
+    if (text.length() == 1 && !isdigit(text[0]))
+        return true;
+    return false;
+}
 
-void ScalarConverter::convert(std::string text) {
+
+void ScalarConverter::convert(std::string &text) {
 
     if (isSpecialLiteral(text))
         return;
-    
-
 
     // Char
+    if (isChar(text))
+    {
+        std::cout << "char: '" << text[0] << "'" << std::endl;
+        std::cout << "int: " << static_cast<int>(text[0]) << std::endl;
+        std::cout << "float: " << static_cast<float>(text[0]) << "f" << std::endl;
+        std::cout << "double: " << static_cast<double>(text[0]) << std::endl;
+    }
+    
     // int
     // float
     // double
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -14,7 +14,7 @@ class ScalarConverter
         ScalarConverter &operator=(const ScalarConverter &other);
         ~ScalarConverter();
     public:
-        static void convert(std::string text);
+        static void convert(std::string &text);
 
 
 };
