af551cf84a86ad528a690df6544118e27110a567
halitykc <87850150+halitykc@users.noreply.github.com>
2026-09-08 17:22:29 +0000

    alsdakj

--- cpp07/ex00/template.hpp
+++ cpp07/ex00/template.hpp
@@ -5,7 +5,24 @@
 
 
 
+template <typename T>
+void swap(T &a, T &b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
 
+template <typename T>
+T min(const T &a, const T &b)
+{   
+    return (a < b ? a : b);
+}
 
+template <typename T>
+T max(const T &a, const T &b)
+{
+    return (a > b ? a : b);
+}
 
 #endif
