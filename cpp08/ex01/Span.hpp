85c839aba5cfb6ff40a1c2f39f15910c63fd04f8
halitykc <87850150+halitykc@users.noreply.github.com>
2026-09-18 17:31:40 +0000

    lksakdj

---
+++ cpp08/ex01/Span.hpp
@@ -0,0 +1,53 @@
+
+
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <vector>
+#include <exception>
+#include <iterator>
+
+
+class Span
+{
+    private:
+        unsigned int _size;
+        std::vector<int> _vector;
+    public:
+
+        Span();
+        Span(unsigned int n);
+        Span(const Span &other);
+        Span &operator=(const Span &other);
+        ~Span();
+
+        void addNumber(int n);
+
+        unsigned int shortestSpan() const;
+        unsigned int longestSpan() const;
+
+        class SpanFullException : public std::exception
+        {
+            public:
+                const char *what() const throw();
+        };
+
+        class NotEnoughNumbersException : public std::exception
+        {
+            public:
+                const char *what() const throw();
+        };
+
+
+
+    template <typename IT>
+    void addNumber(IT begin, IT end)
+    {
+        unsigned int distance = std::distance(begin, end);
+        if (_vector.size() + distance > _size)
+            throw SpanFullException();
+        _vector.insert(_vector.end(), begin, end);
+    }
+};
+
+#endif
